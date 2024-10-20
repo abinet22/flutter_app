ff4874473a4abc1294e1218fd4ba1d9c9b6d7775
abinet22 <abinet22@gmail.com>
2024-10-20 17:28:38 +0300

    resolved the prefer_const_constructors warnings by creating a reusable CustomTextField

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_app/main.dart'; // Replace with your app's import
@@ -7,7 +6,8 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('My first integration test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp()); // Replace MyApp with your app's widget
+    await tester
+        .pumpWidget(const MyApp()); // Replace MyApp with your app's widget
     await tester.pumpAndSettle(); // Wait for animations and builds
 
     // Check if the 'Welcome' text is displayed
